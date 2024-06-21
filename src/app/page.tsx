0bcacc93b3ab02050dad8afe71dfc373916dd41b
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2024-06-21 15:10:31 -0400

    initialized the repository and added assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+      <h1>Legend Has It...a new hi-fi bar is coming soon to Sacramento</h1>
     </main>
   );
 }
