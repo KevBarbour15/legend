96d429b997a182b4d8b3808c5dcec949e285f098
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2024-09-09 19:11:27 -0400

    z- index update

--- src/components/mobile-menu/MobileMenu.tsx
+++ src/components/mobile-menu/MobileMenu.tsx
@@ -45,14 +45,11 @@ const MobileMenu: React.FC = () => {
     >
       <div className="flex flex-grow flex-col justify-start pl-6 pt-6">
         <img
-          className="mb-6 w-[200px] drop-shadow-text md:hidden"
+          className="mb-6 w-[175px] drop-shadow-text md:hidden"
           src="./images/alt-logo.png"
           alt="Logo"
         />
-        <ul
-          className="font-bigola text-5xl uppercase"
-          id="menu-text"
-        >
+        <ul className="font-bigola text-4xl uppercase" id="menu-text">
           {links.map((link, idx) => (
             <li key={idx} className="m-0 p-0">
               <Link href={link.path}>{link.label}</Link>
@@ -67,12 +64,12 @@ const MobileMenu: React.FC = () => {
           rel="noopener noreferrer"
           href="https://www.google.com/maps/dir//410+L+St,+Sacramento,+CA+95814/@38.5798987,-121.5844553,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x809ad12b9928b091:0x8fd24ebe337fbfe7!2m2!1d-121.5020543!2d38.5799276?entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D"
         >
-          <div id="menu-text" className="flex justify-between text-5xl">
+          <div id="menu-text" className="flex justify-between text-4xl">
             <p>410</p>
             <p>L</p>
             <p>St</p>
           </div>
-          <p id="menu-text" className="flex justify-between text-4xl">
+          <p id="menu-text" className="flex justify-between text-3xl">
             <span>S</span>
             <span>a</span>
             <span>c</span>
@@ -84,7 +81,7 @@ const MobileMenu: React.FC = () => {
             <span>t</span>
             <span>o</span>
           </p>
-          <div id="menu-text" className="flex justify-between text-5xl">
+          <div id="menu-text" className="flex justify-between text-4xl">
             <p>CA</p>
             <p>95814</p>
           </div>
