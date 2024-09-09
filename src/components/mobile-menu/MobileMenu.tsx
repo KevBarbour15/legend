681b141b8a9ae3c561536ac4c7c8e8d58318aa05
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2024-09-09 18:12:04 -0400

    test update

--- src/components/mobile-menu/MobileMenu.tsx
+++ src/components/mobile-menu/MobileMenu.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
 import { useEffect, useRef, useState } from "react";
 
 const links = [
@@ -12,11 +11,13 @@ const links = [
 
 const MobileMenu: React.FC = () => {
   const menuRef = useRef<HTMLDivElement>(null);
-  const [windowHeight, setWindowHeight] = useState<number>(window.innerHeight);
+  const [windowHeight, setWindowHeight] = useState<number>(0);
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      // Set initial height when the component mounts
+      // Ensure that we're in the client before accessing window
+      setIsClient(true);
       setWindowHeight(window.innerHeight);
 
       const updateHeight = () => {
@@ -31,20 +32,25 @@ const MobileMenu: React.FC = () => {
     }
   }, []);
 
+  // Render only when on the client side
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <div
       ref={menuRef}
-      style={{ height: windowHeight }}
+      style={{ height: `${windowHeight}px` }} // Dynamically set height
       className="flex flex-col justify-between text-customCream md:hidden"
     >
       <div className="flex flex-grow flex-col justify-start pl-6 pt-6">
         <img
-          className="mb-6 w-[175px] drop-shadow-text md:hidden"
+          className="mb-6 w-[200px] drop-shadow-text md:hidden"
           src="./images/alt-logo.png"
           alt="Logo"
         />
         <ul
-          className="font-bigola text-4xl uppercase drop-shadow-text"
+          className="font-bigola text-5xl uppercase drop-shadow-text"
           id="menu-text"
         >
           {links.map((link, idx) => (
@@ -54,19 +60,19 @@ const MobileMenu: React.FC = () => {
           ))}
         </ul>
       </div>
-      <div className="mb-24 ml-6 w-fit font-bigola drop-shadow-text">
+      <div className="mb-12 ml-6 w-fit font-bigola drop-shadow-text">
         <a
           className="cursor-pointer"
           target="_blank"
           rel="noopener noreferrer"
           href="https://www.google.com/maps/dir//410+L+St,+Sacramento,+CA+95814/@38.5798987,-121.5844553,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x809ad12b9928b091:0x8fd24ebe337fbfe7!2m2!1d-121.5020543!2d38.5799276?entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D"
         >
-          <div id="menu-text" className="flex justify-between text-4xl">
+          <div id="menu-text" className="flex justify-between text-5xl">
             <p>410</p>
             <p>L</p>
             <p>St</p>
           </div>
-          <p id="menu-text" className="flex justify-between text-3xl">
+          <p id="menu-text" className="flex justify-between text-4xl">
             <span>S</span>
             <span>a</span>
             <span>c</span>
@@ -78,7 +84,7 @@ const MobileMenu: React.FC = () => {
             <span>t</span>
             <span>o</span>
           </p>
-          <div id="menu-text" className="flex justify-between text-4xl">
+          <div id="menu-text" className="flex justify-between text-5xl">
             <p>CA</p>
             <p>95814</p>
           </div>
