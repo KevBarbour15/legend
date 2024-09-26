d5ea2f7400b46c100c54ff6eca02310d86fc4ce5
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2024-09-26 16:34:47 -0400

    added events toggle to events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,16 +1,12 @@
 "use client";
-import { useState, useEffect, useRef } from "react";
-
+import { useState, useEffect, useRef, useCallback } from "react";
 import { Event } from "@/types.ts";
-
 import EventCard from "@/components/event-card/EventCard";
 import SideMenu from "@/components/side-menu/SideMenu";
 import MobileHeading from "@/components/mobile-heading/MobileHeading";
-
 import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-
 import { Progress } from "@/components/ui/progress";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function generateProgress(min: number, max: number) {
   return Math.random() * (max - min) + min;
@@ -21,13 +17,19 @@ export default function Events() {
   const [displayEvents, setDisplayEvents] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [activeTab, setActiveTab] = useState<"upcoming" | "past">("upcoming");
   const containerRef = useRef<HTMLDivElement>(null);
-  const eventsTL = useRef<gsap.core.Timeline | null>(null);
-  const eventRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const upcomingEventRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const pastEventRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const upcomingEmptyMessageRef = useRef<HTMLDivElement>(null);
+  const pastEmptyMessageRef = useRef<HTMLDivElement>(null);
   const [progress, setProgress] = useState<number>(0);
 
-  useGSAP(() => {
-    if (!containerRef.current) return;
+  const animateEvents = useCallback(() => {
+    const currentRefs =
+      activeTab === "upcoming" ? upcomingEventRefs : pastEventRefs;
+    const currentEmptyRef =
+      activeTab === "upcoming" ? upcomingEmptyMessageRef : pastEmptyMessageRef;
 
     if (loading) {
       gsap.fromTo(
@@ -37,21 +39,31 @@ export default function Events() {
       );
     }
 
-    gsap.set(eventRefs.current, {
-      x: "50%",
-      opacity: 0,
-    });
-
-    if (!loading && eventRefs.current.length > 0 && displayEvents) {
-      eventsTL.current = gsap.timeline({}).to(eventRefs.current, {
-        delay: 0.15,
-        duration: 0.25,
-        stagger: 0.05,
-        x: 0,
-        opacity: 1,
-      });
+    if (!loading && displayEvents) {
+      if (currentRefs.current.length > 0) {
+        gsap.set(currentRefs.current, { x: "50%", opacity: 0 });
+        gsap.to(currentRefs.current, {
+          delay: 0.15,
+          duration: 0.25,
+          stagger: 0.05,
+          x: 0,
+          opacity: 1,
+        });
+      } else if (currentEmptyRef.current) {
+        gsap.set(currentEmptyRef.current, { opacity: 0, scale: 0.75 });
+        gsap.to(currentEmptyRef.current, {
+          delay: 0.15,
+          duration: 0.25,
+          scale: 1,
+          opacity: 1,
+        });
+      }
     }
-  }, [displayEvents]);
+  }, [loading, displayEvents, activeTab]);
+
+  useEffect(() => {
+    animateEvents();
+  }, [animateEvents, activeTab]);
 
   const fetchEvents = async () => {
     try {
@@ -72,7 +84,6 @@ export default function Events() {
     } finally {
       setTimeout(() => {
         setProgress(100);
-
         setTimeout(() => setLoading(false), 200);
         setTimeout(() => setDisplayEvents(true), 250);
       }, 300);
@@ -82,6 +93,32 @@ export default function Events() {
   useEffect(() => {
     fetchEvents();
   }, []);
+
+  const filterEvents = (type: "upcoming" | "past") => {
+    const now = new Date();
+    return events.filter((event) => {
+      const eventDate = new Date(event.date);
+      return type === "upcoming" ? eventDate >= now : eventDate < now;
+    });
+  };
+
+  const upcomingEvents = filterEvents("upcoming");
+  const pastEvents = filterEvents("past");
+
+  const EmptyMessage = ({
+    message,
+    refProp,
+  }: {
+    message: string;
+    refProp: React.RefObject<HTMLDivElement>;
+  }) => (
+    <div
+      ref={refProp}
+      className="flex h-[75vh] w-full flex-col items-center justify-center opacity-0"
+    >
+      <h2 className="mt-3 font-bigola text-4xl text-customCream">{message}</h2>
+    </div>
+  );
 
   return (
     <>
@@ -105,35 +142,85 @@ export default function Events() {
               className="w-[75vw] max-w-[350px] text-customCream"
             />
           </div>
-        ) : events.length === 0 ? (
-          <div
-            id="event-subheading"
-            className="flex h-[75vh] w-full flex-col items-center justify-center"
-          >
-            <h2 className="mt-3 font-bigola text-4xl text-customCream">
-              Stay tuned for upcoming events!
-            </h2>
-          </div>
         ) : (
-          <div className="w-full">
-            {events.map((event, index) => (
-              <div
-                key={event._id}
-                ref={(el) => {
-                  eventRefs.current[index] = el;
-                }}
-                className={`w-full border-customGold opacity-0 ${index === 0 ? "md:border-t" : "border-t"} ${
-                  index === events.length - 1 ? "border-b" : ""
-                }`}
-              >
-                <EventCard
-                  fetchEvents={fetchEvents}
-                  key={index}
-                  event={event}
-                />
-              </div>
-            ))}
-          </div>
+          <Tabs
+            defaultValue="upcoming"
+            className="flex w-full flex-col items-center"
+            onValueChange={(value) =>
+              setActiveTab(value as "upcoming" | "past")
+            }
+          >
+            <TabsList className="my-3 grid w-full grid-cols-2 font-bigola md:mb-6 md:mt-0 md:w-[400px]">
+              <TabsTrigger value="upcoming">Upcoming Events</TabsTrigger>
+              <TabsTrigger value="past">Past Events</TabsTrigger>
+            </TabsList>
+            <TabsContent
+              value="upcoming"
+              className="w-full border-t border-customGold md:border-0"
+            >
+              {upcomingEvents.length > 0 ? (
+                <div>
+                  {upcomingEvents.map((event, index) => (
+                    <div
+                      key={event._id}
+                      ref={(el) => {
+                        upcomingEventRefs.current[index] = el;
+                      }}
+                      className={`w-full border-customGold opacity-0 ${index === 0 ? "md:border-t" : "border-t"} ${
+                        index === upcomingEvents.length - 1 ? "border-b" : ""
+                      }`}
+                    >
+                      <EventCard
+                        fetchEvents={fetchEvents}
+                        key={index}
+                        event={event}
+                      />
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <div className="text-center text-xl md:text-3xl">
+                  <EmptyMessage
+                    message="Stay tuned for upcoming events!"
+                    refProp={upcomingEmptyMessageRef}
+                  />
+                </div>
+              )}
+            </TabsContent>
+            <TabsContent
+              value="past"
+              className="w-full border-t border-customGold md:border-0"
+            >
+              {pastEvents.length > 0 ? (
+                <div>
+                  {pastEvents.map((event, index) => (
+                    <div
+                      key={event._id}
+                      ref={(el) => {
+                        pastEventRefs.current[index] = el;
+                      }}
+                      className={`w-full border-customGold opacity-0 ${index === 0 ? "md:border-t" : "border-t"} ${
+                        index === pastEvents.length - 1 ? "border-b" : ""
+                      }`}
+                    >
+                      <EventCard
+                        fetchEvents={fetchEvents}
+                        key={index}
+                        event={event}
+                      />
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <div className="text-center text-xl md:text-3xl">
+                  <EmptyMessage
+                    message="No past events to display."
+                    refProp={pastEmptyMessageRef}
+                  />
+                </div>
+              )}
+            </TabsContent>
+          </Tabs>
         )}
       </div>
     </>
