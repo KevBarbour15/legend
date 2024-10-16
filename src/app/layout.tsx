4a1388a85268a437f4fbecd824d0b0d6b0e4b2f0
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2024-10-16 18:31:40 -0400

    updated id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,6 +73,7 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <script
+          id="mcjs"
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/783ca62931283b3104dae7fdb/5f76e1f7d9803b38a26cab6bc.js");`,
