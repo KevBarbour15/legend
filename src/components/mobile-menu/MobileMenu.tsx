103855b2e86d78fb0c3253eaf35fb0d3547b431c
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2025-01-29 16:45:49 -0500

    reset on height

--- src/components/mobile-menu/MobileMenu.tsx
+++ src/components/mobile-menu/MobileMenu.tsx
@@ -49,10 +49,10 @@ const MobileMenu: React.FC = () => {
   return (
     <div
       ref={menuRef}
-      className="relative flex h-screen flex-col justify-between text-customCream md:hidden"
+      className="relative flex h-svh flex-col justify-between text-customCream md:hidden"
       id="mobile-menu"
     >
-      <div className="fixed flex flex-grow flex-col justify-start pl-3 pt-3">
+      <div className="absolute flex flex-grow flex-col justify-start pl-3 pt-3">
         <Image
           className="w-[150px] md:hidden"
           src="/images/alt-logo.png"
@@ -89,7 +89,7 @@ const MobileMenu: React.FC = () => {
           ))}
         </ul>
       </div>
-      <div className="mobile-link fixed bottom-0 left-0 ml-3 w-fit pb-20 opacity-0">
+      <div className="mobile-link absolute bottom-0 left-0 ml-3 w-fit pb-20 opacity-0">
         <a
           className="cursor-pointer font-bigola"
           target="_blank"
