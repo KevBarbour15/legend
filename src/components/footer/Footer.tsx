3f2babb578b4ffcf27b6e68786eaf1682f135dd0
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2025-08-02 13:15:27 -0400

    added

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Image from "next/image";
 import { Copyright, Phone, Key } from "@phosphor-icons/react";
 
 import { FaYelp, FaYoutube, FaFacebook, FaSpotify } from "react-icons/fa";
@@ -11,8 +11,8 @@ import MailchimpForm from "@/components/mailchimp-form/MailchimpForm";
 
 const Footer = () => {
   return (
-    <footer className="shadow-top rounded-t-md border-x border-t border-customNavy/20 bg-[#f5f5f5]/25 p-3 pt-6 backdrop-blur-[1px] md:p-6">
-      <div className="flex flex-col-reverse items-center gap-6 lg:flex-row lg:gap-0">
+    <footer className="shadow-top relative overflow-hidden rounded-t-md border-x border-t border-customNavy/20 bg-[#f5f5f5]/25 p-3 pt-6 backdrop-blur-[1px] md:p-6">
+      <div className="z-10 flex flex-col-reverse items-center gap-6 lg:flex-row lg:gap-0">
         <div className="flex w-full md:basis-1/2">
           <MailchimpForm setShouldShow={() => {}} />
         </div>
