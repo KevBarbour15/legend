65180408334061e495c7b0efa54e7b78bcac59d4
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2026-02-18 11:16:41 -0500

    update

--- src/app/shop/ShopContent.tsx
+++ src/app/shop/ShopContent.tsx
@@ -15,14 +15,13 @@ export default function ShopContent({ products }: { products: any[] }) {
 
     const tl = gsap.timeline();
 
-    tl.set(gridRef.current, { opacity: 0, y: 25 })
-      .to(gridRef.current, {
-        y: 0,
-        opacity: 1,
-        stagger: 0.05,
-        duration: 0.5,
-        ease: "back.out(1.7)",
-      });
+    tl.set(gridRef.current, { opacity: 0, y: 25 }).to(gridRef.current, {
+      y: 0,
+      opacity: 1,
+      stagger: 0.05,
+      duration: 0.5,
+      ease: "back.out(1.7)",
+    });
   }, []);
 
   return (
@@ -32,7 +31,7 @@ export default function ShopContent({ products }: { products: any[] }) {
         <div className="mx-auto h-full px-3 pb-12 pt-6 md:pb-6 md:pl-[240px] md:pr-6 xl:max-w-[1280px] xxl:max-w-[1536px]">
           <div
             ref={gridRef}
-            className="grid w-full grid-cols-2 gap-4 opacity-0 sm:grid-cols-2 md:px-0 lg:gap-8 2xl:grid-cols-3"
+            className="grid w-full grid-cols-1 gap-4 opacity-0 sm:grid-cols-2 md:px-0 lg:gap-8 2xl:grid-cols-3"
           >
             {products.map((product) => (
               <ProductCard key={product.id} product={product} />
