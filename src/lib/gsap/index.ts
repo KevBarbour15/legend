21bba0c57c5dad952c22e3f3508629e650a2aa92
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2026-05-19 14:26:58 -0400

    update

--- src/lib/gsap/index.ts
+++ src/lib/gsap/index.ts
@@ -1,6 +1,6 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { SplitText } from "gsap/dist/SplitText";
+import { SplitText } from "gsap/SplitText";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(useGSAP, SplitText, ScrollTrigger);
